cd338cf88dce5a2a93adc2881e6cf81203563470
Carl Schoonover <ces2001@cumc.columbia.edu>
2016-12-16 10:54:07 -0500

    ui

--- UIdesign.py
+++ UIdesign.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'C:\Users\All\Desktop\pyscript\eclipse\VBAcmd\UIdesign.ui'
 #
-# Created: Tue Dec 13 12:08:20 2016
+# Created: Wed Dec 14 08:42:52 2016
 #      by: PyQt4 UI code generator 4.9.6
 #
 # WARNING! All changes made in this file will be lost!
